47d7204b2bd2ddd3096d9d5ceb0e4256771b20fa
Bramzz-id <bramantasurya67@gmail.com>
2026-09-05 11:02:07 +0800

    add gh-pages workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/undangan-pernikahan/', // Tambahkan baris ini (sesuai nama repositori di GitHub)
   plugins: [
     react(),
     tailwindcss(),

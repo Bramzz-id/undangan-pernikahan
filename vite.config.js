1a7a0a33732dd86f36f84ad60ac614536bf009f4
Bramzz-id <bramantasurya67@gmail.com>
2026-09-07 21:59:17 +0800

    update untuk vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/undangan-pernikahan/', // Tambahkan baris ini (sesuai nama repositori di GitHub)
+  // base: '/undangan-pernikahan/', 
   plugins: [
     react(),
     tailwindcss(),
